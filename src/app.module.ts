4a37c551cd6f167d4479387992bcdc39dd162835
Felipe Decome <decome.felipe@gmail.com>
2021-12-09 15:18:40 -0300

    feat: creating orders and setting docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+// import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Order } from './orders/entities/order.entity';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [
+    OrdersModule,
+    // SequelizeModule.forRoot({
+    //   dialect: 'sqlite',
+    //   host: join(__dirname, 'data', 'database.sqlite'),
+    //   models: [Order],
+    //   autoLoadModels: true,
+    // }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'db',
+      port: 3306,
+      database: 'api_full_cycle',
+      username: 'root',
+      password: 'root',
+      models: [Order],
+      autoLoadModels: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
